2ff5939a67ddb5367a0f78eb74afc1bd76cbbb15
xerovx74 <60182875+xerovx74@users.noreply.github.com>
2023-04-10 19:08:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const { createApi } = require('openai');
-
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const openai = createApi(process.env.OPENAI_API_KEY);
+const openai = require("openai");
+const openaiApi = new openai.default({apiKey: process.env.OPENAI_API_KEY});
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async msg => {
-  if (msg.content.startsWith('!chatgpt')) {
+client.on("message", async (msg) => {
+  if (msg.content.startsWith("!chatgpt")) {
     const message = msg.content.slice(9).trim();
     const response = await openai.complete({
-      engine: 'davinci-codex',
+      engine: "davinci-codex",
       prompt: message,
       maxTokens: 1024,
       n: 1,
-      stop: ['\n']
+      stop: ["\n"],
     });
     msg.reply(response.data.choices[0].text);
   }
